d2686e93a5975e7a84421bcabfaf173efd68c374
Diakonen <oscar_pehrson@msn.com>
2016-02-23 10:26:57 +0100

    Database klass.

---
+++ WebApplication5/Models/TheDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace WebApplication5.Models
+{
+    public class TheDatabase
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+    }
+}
