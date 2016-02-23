ac2de323ed9eea7d4f33c18e0a85c3aed2b560ed
Diakonen <oscar_pehrson@msn.com>
2016-02-23 10:23:58 +0100

    Skapat modellen

---
+++ WebApplication5/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public DateTime BeginDateTime { get; set; }
+        public DateTime DeadLineDate { get; set; }
+        public string Title { get; set; }
+        public string Req { get; set; }
+    }
+}
